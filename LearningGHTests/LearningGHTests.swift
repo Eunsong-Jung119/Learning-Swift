5a76054dd9fb728fa22ef3e5f2dfcb07c8b96342
Eunsong-Jung119 <eunsongjung1997@gmail.com>
2025-01-16 22:08:13 +0900

    Initial Commit

---
+++ LearningGHTests/LearningGHTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHTests.swift
+//  LearningGHTests
+//
+//  Created by Eunsong Jung on 1/16/25.
+//
+
+import Testing
+@testable import LearningGH
+
+struct LearningGHTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
